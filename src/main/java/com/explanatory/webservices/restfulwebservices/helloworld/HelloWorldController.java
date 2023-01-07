197fa092968fcdb9c7d1d67be32bede3e3e3bf4b
Ajay Tidke <ajaytidke28297@gmail.com>
2023-01-07 21:43:26 +0530

    Versioning of api

--- src/main/java/com/explanatory/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/explanatory/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,12 +1,20 @@
 package com.explanatory.webservices.restfulwebservices.helloworld;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping(path = "hello-world")
     public String helloWorld() {
@@ -21,6 +29,18 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World! %s", name));
+    }
+
+    @GetMapping(path = "hello-world-inter")
+    public String helloWorldInter() {
+
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage(
+                "good.morning.message",
+                null,
+                "Default Message",
+                locale
+        );
     }
 
 }
